caf02b0d43c8ced7523bcbbda69fa5e27113cfbe
HanChung-Wu <205856604+HanChung-Wu@users.noreply.github.com>
2025-09-09 15:34:12 +0800

    feat(electron): Update Electron main process with enhanced window configuration and cleanup; remove unused data files

--- electron/main.ts
+++ electron/main.ts
@@ -3,7 +3,17 @@ import path from "path";
 import { isDevelopment } from "./utils.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  console.log(`This platform is ${process.platform}`);
+
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(app.getAppPath(), "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
 
   // Load the Vite dev server URL
   if (isDevelopment()) {
@@ -11,5 +21,12 @@ app.on("ready", () => {
   } else {
     // Load the local file for production builds
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    console.log("App closed");
   }
 });
