8029d0aadb12cb170cdd6cc332090d3c9b2d627d
HanChung-Wu <205856604+HanChung-Wu@users.noreply.github.com>
2025-09-09 14:47:17 +0800

    feat(electron): Enhance Electron setup with improved TypeScript configuration and routing adjustments

--- electron/main.ts
+++ electron/main.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDevelopment } from "./utils.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
 
   // Load the Vite dev server URL
-  if (process.env.NODE_ENV === "development") {
+  if (isDevelopment()) {
     mainWindow.loadURL("http://localhost:5173"); // Replace with your Vite dev server port
   } else {
     // Load the local file for production builds
